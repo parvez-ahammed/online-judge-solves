a67c9e3878bad7e1d9673bfdead8663d0b525db0
piru72 <parvezdirom2000@gmail.com>
2022-06-04 02:39:33 +0600

    contest_1

---
+++ academy/F_Digits_Summation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   long long a ,b ;
+   cin >> a>>b ;
+   cout << ((a%10) + (b%10)) <<endl;
+
+
+    return 0;
+}
---
+++ academy/S_Interval.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/**-------Shortend Library-------*/
+
+#define vi vector<int>
+#define mii map<int, int>
+
+#define ff first
+#define ss second
+#define all(a) a.begin(), a.end()
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+
+#define SortA(ar, s) sort(ar, ar + s)
+#define SortD(ar, s) sort(ar, ar + s, greater<int>())
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+
+#define leftShift(p) (p << 1)
+#define rightShift(p) (p >> 1)
+
+#define min3(a, b, c) min(a, min(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max3(a, b, c) max(a, max(b, c))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb(x) push_back(x)
+#define mod 1000000007
+#define precision(x) cout << fixed << setprecision(x)
+
+/**----data type----*/
+#define ll long long int
+#define llu unsigned long long int
+
+/**----Functions to use----**/
+
+ll powerLog(ll base, ll power)
+{
+    ll res = 1;
+    while (power)
+    {
+        if (power % 2)
+        {
+            res *= base;
+            power--;
+        }
+        else
+        {
+            base *= base;
+            power /= 2;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    double a;
+
+    cin >> a;
+
+    if (a >= 0  && a<= 25)
+    {
+        cout << "Interval [0,25]" <<endl;
+    }
+    else if (a> 25 && a <= 50)
+    {
+         cout << "Interval (25,50]" <<endl;
+    }
+    else if (a> 50 && a <= 75)
+    {
+         cout << "Interval (50,75]" <<endl;
+    }
+    else if (a> 75 && a <= 100)
+    {
+         cout << "Interval (75,100]" <<endl;
+    }
+    else 
+    {
+
+        cout << "Out of Intervals" <<endl;
+    }
+
+    return 0;
+}
