2689c2e527af9c0403c01ffd5b8ca374ba684d3a
piru72 <parvezdirom2000@gmail.com>
2022-08-22 04:42:59 +0600

    toph problems solved

--- Toph/Caesar_Cipher.cpp
+++ Toph/Caesar_Cipher.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-
-// C++ program to demonstrate getline() function
-
-#include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
-    string str;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n;
     cin >> n;
-
+    string str;
+    cin.ignore();
     getline(cin, str);
+    
+    
+   int temp_n;
+
+    for ( int i = 0 ; i < str.size() ;i++)
+    {
+        temp_n = n;
+        if (str[i]!= ' ')
+        {
+            if (str[i] - n >= 'a')
+            {
+                str[i] -= n;
+            }
+            else 
+            {
+                int morels = str[i]- 'a';
+                temp_n-=morels;
+                str[i] = 'z'-temp_n+1;
+               
+            }
+            
+
+        }
+    }
+
     cout << str;
 
     return 0;
