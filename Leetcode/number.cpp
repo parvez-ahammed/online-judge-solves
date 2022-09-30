5db5ee62603088fbd50c9ccc2a3247efa007923d
piru72 <parvezdirom2000@gmail.com>
2022-09-30 20:41:46 +0600

    ~decreases for loop timing by 10s

---
+++ Leetcode/number.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int ans = 0, num = 0;
+    vector<int> val(200, 0);
+
+
+    cout << s[0]-0 << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
