e93460568ce6ff1ed4a0b794b440545b2a2f6dab
piru72 <parvezdirom2000@gmail.com>
2022-10-12 22:19:58 +0600

    problems initialized

---
+++ codeforces/newcomers_training/contest_1/A_Winter_Sale.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+    double x, n;
+    cin >> x >> n;
+    cout << fixed << setprecision(2) << (100.0 * n) / (100.0 - x) << "\n";
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/newcomers_training/contest_1/B_Memo_and_Momo.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+    ll a, b, k;
+    cin >> a >> b >> k;
+
+
+    if (a % k == 0 && b % k == 0)
+        cout
+            << "Both"
+            << "\n";
+    else if (a % k == 0 && b % k != 0)
+        cout
+            << "Memo"
+            << "\n";
+    else if (a % k != 0 && b % k == 0)
+        cout
+            << "Momo"
+            << "\n";
+    else
+        cout << "No One"
+             << "\n";
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
