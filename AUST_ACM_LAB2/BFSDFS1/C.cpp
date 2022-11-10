b7338cf936eb98c665e6a41e68bb611b4349934f
piru72 <parvezdirom2000@gmail.com>
2022-11-10 06:29:42 +0600

    c solved

--- AUST_ACM_LAB2/BFSDFS1/C.cpp
+++ AUST_ACM_LAB2/BFSDFS1/C.cpp
@@ -10,8 +10,10 @@ using namespace std;
 #define sq(x) (x) * (x)
 #define mid(l, r) (l + (r - l) / 2)
 #define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
-int fx[] = {+0, +0, +1, -1};
-int fy[] = {-1, +1, +0, +0};
+int dx[] = {+0, +0, +1, -1};
+int dy[] = {-1, +1, +0, +0};
+
+
 
 void showVector(vector<vector<int>> vv)
 {
@@ -32,6 +34,7 @@ void solve(int row, int col)
 {
 
     vector<vector<int>> vv(row, vector<int>(col, 0));
+    vector<vector<int>> pp = vv;
 
     int number_of_bomb_row = 0;
     cin >> number_of_bomb_row;
@@ -51,12 +54,60 @@ void solve(int row, int col)
     int src_x = 0, src_y = 0, des_x = 0, des_y = 0;
     cin >> src_x >> src_y >> des_x >> des_y;
 
-    showVector(vv);
+    queue<pair<int, pair<int, int>>> s;
 
-    cout << isValid(-1, -1, row, col) << endl;
+    s.push(make_pair(0, make_pair(src_x, src_y)));
+
+    int final_cost = 0;
+    pp[src_x][src_y] = 1;
+
+    while (!s.empty())
+    {
+        int current_cost = s.front().first;
+        int current_x = s.front().second.first;
+        int current_y = s.front().second.second;
+         s.pop();
+
+        // cout << "FOR SOURCE   " << current_x << current_y << "   VALID ADJ ARE"   << "WTITH COST " <<  current_cost << endl;
+
+        if (current_x == des_x && current_y == des_y)
+        {
+            final_cost = current_cost;
+            break;
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            int adj_x = current_x + dx[i];
+            int adj_y = current_y + dy[i];
+
+            //  cout << "ADJ VALUE IS " << adj_x << adj_y << endl;
+
+            if (isValid(adj_x, adj_y, row, col))
+            {
+                // cout << "PP VALUE IS " << pp[adj_x][adj_y] << "   " << adj_x << adj_y << endl;
+
+                if (vv[adj_x][adj_y] != 1)
+                {
+
+                    if (pp[adj_x][adj_y] == 0)
+                    {
+                        // cout << "ADJ X " << adj_x << " ADJ Y " << adj_y << " COST " << current_cost << endl;
+                        s.push(make_pair(current_cost + 1, make_pair(adj_x, adj_y)));
+                        pp[adj_x][adj_y] = 1;
+                    }
+                }
+            }
+        }
+       
+
+        // cout << endl;
+    }
 
     // cout << src_x << src_y << " " << des_x << des_y << endl;
-    cout << endl;
+
+    //  showVector(pp);
+    cout << final_cost << endl;
 }
 
 int32_t main()
