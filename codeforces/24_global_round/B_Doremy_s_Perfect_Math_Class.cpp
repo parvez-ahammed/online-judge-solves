29c3ec3409089c11ce56ba706fa859852b2cb04b
piru72 <parvezdirom2000@gmail.com>
2022-11-26 23:12:18 +0600

    3 solvedd

---
+++ codeforces/24_global_round/B_Doremy_s_Perfect_Math_Class.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+
+    int temp, all_gcd = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        all_gcd = __gcd(all_gcd, temp);
+    }
+    
+    cout << temp / all_gcd << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
