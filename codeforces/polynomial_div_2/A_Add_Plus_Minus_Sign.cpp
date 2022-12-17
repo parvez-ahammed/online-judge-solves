3ed6f1a4b8e46b0e069eb7d7ee75f89765cc471a
piru72 <parvezdirom2000@gmail.com>
2022-12-17 22:54:24 +0600

    solved but b hacked

---
+++ codeforces/polynomial_div_2/A_Add_Plus_Minus_Sign.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+
+    int prev_pos = 0;
+    int one_count = 0;
+    if (str[0] == '1')
+        one_count++;
+
+    for (int i = 1; i < str.size(); i++)
+    {
+        if (str[i] == '0')
+            cout << '+';
+        else if ((one_count + 1) % 2 == 0)
+        {
+            cout << '-';
+            one_count++;
+        }
+        else
+        {
+            cout << '+';
+            one_count++;m,  80
+        }
+    }
+
+    cout << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/polynomial_div_2/C_Ice_and_Fire.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    vector<int> v;
+    v.push_back(0);
+
+    cout << 1 << " ";
+
+    int counter = 1;
+
+    for (int i = 1; i < n-1 ; i++)
+    {
+        if (str[i - 1] == str[i])
+
+        {
+            cout << 1 << " ";
+            counter ++;
+        }
+        else 
+        {
+            counter++;
+            cout << counter << " ";
+            counter = 1;
+        }
+    }
+
+    cout << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
