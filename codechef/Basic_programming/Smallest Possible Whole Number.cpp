5556c86ae65d466a2f0646653ea059039529f27a
piru72 <parvezdirom2000@gmail.com>
2022-12-23 21:20:09 +0600

    learning : check for mod by 0 always

---
+++ codechef/Basic_programming/Smallest Possible Whole Number.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b && b!= 0)
+        cout << ( a- b) % b << endl;
+    else if (a == b)
+        cout << 0 << endl;
+    else
+        cout << a << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
