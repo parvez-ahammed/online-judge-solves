30133701caa3c57ba7de10856debc82d168bfae9
piru72 <parvezdirom2000@gmail.com>
2022-12-28 23:33:14 +0600

    starter72 done

---
+++ codechef/PREFIXFLIP.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    string str;
+    cin >> str;
+
+    int curr_size = 0;
+    int max_size = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == '1')
+            curr_size++;
+        else
+        {
+            max_size = max(max_size, curr_size);
+            curr_size = 0;
+        }
+    }
+
+    max_size = max(max_size, curr_size);
+
+    if (k == max_size)
+        cout << 0 << endl;
+    else
+    {
+        int count = 0;
+        for (int i = 0; i < k - 1; i++)
+        {
+            if (str[i] == '0' && str[i] != str[i + 1])
+            {
+                count++;
+            }
+
+            cout << str[i];
+        }
+
+        
+
+        cout << count<<endl;
+
+        if (count == 0)
+            cout << 1 << endl;
+        else
+            cout << count * 2 << endl;
+    }
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
