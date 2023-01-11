774a0453b15d5c22348ec0207b1d281b7023fe69
piru72 <parvezdirom2000@gmail.com>
2023-01-11 22:58:53 +0600

    equations mod turns into mul

--- codechef/Find an integer.cpp
+++ codechef/Find an integer.cpp
@@ -9,26 +9,29 @@ using namespace std;
 #define Reverse(x) reverse(x.begin(), x.end())
 #define sq(x) (x) * (x)
 #define mid(l, r) (l + (r - l) / 2)
-#define binary_of(x) (bitset<32>(x).to_string().substr(31 - log2(x)))
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
 int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
 int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
 
 void solve()
 {
-    ll n, y;
-    cin >> n >> y;
 
-    int temp;
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> temp;
-        ans |= temp;
-    }
+    ll a, b;
+    cin >> a >> b;
+    ll n = a * b - a - b;
 
-    int final_ans = y - ans;
-    ((final_ans & y) == final_ans) ? cout << final_ans : cout << -1;
-    cout << endl;
+    // if (n == -1)
+    //     n = max(a, b) - 1;
+    // if (n == 0)
+    //     n = 2;
+
+    // cout << n << endl;
+
+    // cout << (a * b) / __gcd(a, b) << endl;
+    if (a > 2 && b > 2)
+        cout << (max(a, b) * __gcd(a, b)) - a - b << endl;
+    else
+        cout << 2 << endl;
 }
 
 int32_t main()
