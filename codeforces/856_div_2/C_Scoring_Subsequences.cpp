0d6b1446d8d42308aa05132b97b7b34bc8ff6cae
piru72 <parvezdirom2000@gmail.com>
2023-03-12 23:25:58 +0600

    well be a little bit faster

---
+++ codeforces/856_div_2/C_Scoring_Subsequences.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+#define BIG_MOD 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    vll v(n);
+
+    for (auto &x : v)
+        cin >> x;
+
+    for (int k = 1; k <= n; k++)
+    {
+        int low = 1, high = k;
+
+        while (low < high)
+        {
+            int mid = low + (high - low + 1) / 2;
+
+            if (v[k - mid] >= mid)
+                low = mid;
+            else
+                high = mid - 1;
+
+               // cout << low << " " << high << endl;
+        }
+
+        cout << low << " ";
+    }
+
+    cout << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
