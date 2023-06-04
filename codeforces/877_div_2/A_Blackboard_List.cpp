6c138976dd28ba84d5373445109001f40e1a3731
piru72 <parvezdirom2000@gmail.com>
2023-06-04 22:50:07 +0600

    added (code ) : contest

---
+++ codeforces/877_div_2/A_Blackboard_List.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+#define BIG_MOD 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+
+void solve()
+{
+
+    ll n;
+    cin >> n;
+    vll v(n);
+
+    cin >> v[0];
+    ll minV = v[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        cin >> v[i];
+        minV = min(v[i], minV);
+    }
+    if (minV >= 0)
+    {
+        Sort(v);
+        minV = v[n - 1];
+    }
+
+    cout << minV << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
