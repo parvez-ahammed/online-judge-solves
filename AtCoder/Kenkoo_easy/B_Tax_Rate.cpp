be2cb941b3470bb755557ef80a75d2c7f7d90a86
piru72 <parvezdirom2000@gmail.com>
2023-06-20 20:23:39 +0600

    problem solved

---
+++ AtCoder/Kenkoo_easy/B_Tax_Rate.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+#define BIG_MOD 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define rep(start, x) for (int i = start; i < x; i++)
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int lower_bound = (n / 1.08) - 2;
+    int upper_bound = (n / 1.08) + 2;
+
+    rep(lower_bound, upper_bound)
+    {
+        if (floor(i * 1.08) == n)
+        {
+            cout << i << endl;
+            return;
+        }
+    }
+
+    cout << ":(" << endl;
+    
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
