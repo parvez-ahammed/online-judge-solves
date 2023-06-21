7ac55cc5e56134ddb91562aa99a674c691aad47a
piru72 <parvezdirom2000@gmail.com>
2023-06-21 12:58:22 +0600

    read problems carefully

---
+++ AtCoder/Kenkoo_easy/B_Can_you_solve_this.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+#define BIG_MOD 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define rep(start, x) for (int i = start; i < x; i++)
+
+void solve()
+{
+    int n, m, c;
+    cin >> n >> m >> c;
+
+    ll count = 0;
+
+    vi b(m + 1);
+    rep(0, m) cin >> b[i];
+
+    rep(0, n)
+    {
+
+        ll sum = 0;
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            cin >> x;
+            sum += (x * b[j]);
+        }
+
+        sum += c;
+
+        sum > 0 ? count++ : count = count;
+    }
+
+    cout << count << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
