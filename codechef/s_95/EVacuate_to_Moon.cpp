dc06deba5a4ba5a16f60893cca9f11226f0f9a6f
piru72 <parvezdirom2000@gmail.com>
2023-06-21 21:48:45 +0600

    think simply have confidence

---
+++ codechef/s_95/EVacuate_to_Moon.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+#define BIG_MOD 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define rep(start, x) for (int i = start; i < x; i++)
+
+void solve()
+{
+
+    ll n, m, h;
+    cin >> n >> m >> h;
+
+    vll a(n), b(m);
+
+    rep(0, n) cin >> a[i];
+    rep(0, m) cin >> b[i];
+
+    SortR(a);
+    SortR(b);
+
+    ll sum = 0;
+
+    rep(0, min(n, m))
+        sum += min(a[i], h * b[i]);
+
+    cout<< sum << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ codechef/s_95/Transfusion_Chain.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**----data type----*/
+#define ll long long int
+#define endl "\n"
+#define llu unsigned long long int
+#define Sort(x) sort(x.begin(), x.end())
+#define SortR(x) sort(x.begin(), x.end(), greater<int>())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define sq(x) (x) * (x)
+#define mid(l, r) (l + (r - l) / 2)
+#define binary_of(x) (bitset<32>(x).to_string().substr(32 - log2(x)))
+int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1};
+int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1};
+#define BIG_MOD 1000000007
+#define vi vector<int>
+#define vll vector<ll>
+#define rep(start, x) for (int i = start; i < x; i++)
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    string str = "";
+    ll da = 0, db = 0, dab = 0, dO = 0;
+    rep(0, n)
+    {
+        string x;
+        cin >> x;
+        
+        (x == "A") ? (da++) : ((x == "B") ? (db++) : ((x == "AB") ? dab++ : dO++));
+    }
+
+    ll option1 = dO + dab + da;
+    ll option2 = dO + dab + db;
+
+
+    cout << max(option1 , option2) << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
