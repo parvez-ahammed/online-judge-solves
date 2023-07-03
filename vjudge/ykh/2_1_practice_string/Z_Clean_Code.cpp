8f98d1c4fb3743c465fcac0fb69122e114252766
piru72 <parvezdirom2000@gmail.com>
2023-07-03 13:32:11 +0600

    update

--- vjudge/ykh/2_1_practice_string/Z_Clean_Code.cpp
+++ vjudge/ykh/2_1_practice_string/Z_Clean_Code.cpp
@@ -6,36 +6,46 @@ int main()
     string s;
     stack<char> st;
 
-    while (getline(cin, s)){
+    while (getline(cin, s))
+    {
 
         string line = "";
-        for (int i = 0; i < s.size(); i++){
+        for (int i = 0; i < s.size(); i++)
+        {
 
-            if (i + 1 < s.size()){
-                if (s[i] == s[i + 1] && s[i] == '/' && st.empty()){
+            if (i + 1 < s.size())
+            {
+                if (s[i] == s[i + 1] && s[i] == '/' && st.empty())
+                {
                     break;
                 }
 
-                if (s[i + 1] == '*' && s[i] == '/'){
+                if (s[i + 1] == '*' && s[i] == '/')
+                {
                     st.push('/'), st.push('*');
                 }
             }
 
-            if (i - 1 >= 0){
-                if (s[i - 1] == '*' && s[i] == '/' && st.size() >= 2){
-                    st.pop(), st.pop();
+            if (i - 1 >= 0)
+            {
+                if (s[i - 1] == '*' && s[i] == '/' && st.size() >= 2)
+                {
+
+                    while (!st.empty())
+                        st.pop();
+
                     continue;
                 }
             }
-                
 
-            if (st.empty()){
+            if (st.empty())
+            {
                 line += s[i];
             }
-                
         }
 
-        if (line.size() != 0 && count(line.begin(), line.end(), ' ') != line.size()){
+        if (line.size() != 0 && count(line.begin(), line.end(), ' ') != line.size())
+        {
             cout << line << endl;
         }
     }
