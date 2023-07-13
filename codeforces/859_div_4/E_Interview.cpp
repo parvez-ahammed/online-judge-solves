b4800321c510b90c0a740a97d335e631847e9bbf
piru72 <parvezdirom2000@gmail.com>
2023-07-13 17:07:43 +0600

    idleness limit exceeded

--- codeforces/859_div_4/E_Interview.cpp
+++ codeforces/859_div_4/E_Interview.cpp
@@ -1,74 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 void solve()
 {
 
     int n;
     cin >> n;
-    vector <int > v (n+1);
-    vector <long long > vp(n+1,0);
-    
+    vector<int> v(n + 1);
+    vector<long long> vp(n + 1);
 
-    for(int i =1 ; i <= n ; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> v[i];
-        vp[i] += vp[i-1] + v[i];
-        //cout << vp[i] << " ";
+        vp[i] += vp[i - 1] + v[i];
     }
-    //cout << endl;
-
-
 
     int l = 1, r = n;
     int counter = 0;
-    while (l != r) {
+    int response;
+    while (r - l > 1)
+    {
         int mid = (l + r + 1) / 2;
-        cout << "L " << l << " R " << r  << " MID " << mid << endl;
-        printf("? %d ", (mid-l) +1 );
+        cout << "? " << (mid - l) + 1 << " ";
 
-        for (int i = l ; i <=  l+ (mid-l)   ; i++)
-        {
-             printf("%d ", i);
-        }
-        printf("\n");
-        fflush(stdout);
-        
+        for (int i = l; i <= l + (mid - l); i++)
+            cout << i << " ";
+        cout << endl;
 
-        int response;
-        scanf("%d", &response);
+        cin >> response;
 
-       
-        
-        long long rangeSum = vp[mid] - vp[l-1];
-         cout << response << " "<< rangeSum<< endl;
-         printf("\n\n");
+        long long rangeSum = vp[mid] - vp[l - 1];
 
-        
         if (response == rangeSum)
-            l = mid ;
+            l = mid;
         else
-            r = mid-1;
-
-            counter ++;
-            if (counter > 5)break;
-
-         
+            r = mid - 1;
             
+        counter++;
+        if (counter == 30)
+            return;
     }
 
-    printf("! %d\n\n",l);
+    cout << "? " << 1 << " " << l << endl;
+    cin >> response;
+
+    if (response != v[l])
+    {
+        cout << "! " << l << endl;
+        return;
+    }
+
+    cout << "? " << 1 << " " << r << endl;
+    cin >> response;
+    if (response != v[r])
+        cout << "! " << r << endl;
 }
-int main() {
+int main()
+{
 
     int tc;
     cin >> tc;
-    while(tc--)
-    {
+    while (tc--)
         solve();
-    }
-    
-    
-    fflush(stdout);
 }
