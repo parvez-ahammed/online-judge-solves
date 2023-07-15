b611a20d0a870f103ae86e49b08003f27830fe80
piru72 <parvezdirom2000@gmail.com>
2023-07-15 10:14:12 +0600

    solved first dfs in cf on my own

---
+++ codeforces/790_div_4/G_White_Black_Balanced_Subtrees.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define endl "\n"
+
+#define vi vector<int>
+
+#define rep(start, x) for (int i = start; i < x; i++)
+
+vector<vector<int>> tree;
+vector<pair<int, int>> subtreeColor;
+string color;
+
+pair<int, int> dfs(int root)
+{
+    color[root] == 'W' ? subtreeColor[root].first++ : subtreeColor[root].second++;
+
+    for (int i = 0; i < tree[root].size(); i++)
+    {
+        auto p = dfs(tree[root][i]);
+        subtreeColor[root].first += p.first;
+        subtreeColor[root].second += p.second;
+    }
+
+    return subtreeColor[root];
+}
+
+void solve()
+{
+    int n;
+    tree = vector<vector<int>>(5000);
+    subtreeColor = vector<pair<int, int>>(5000);
+    int x;
+
+    cin >> n;
+
+    rep(2, n + 1) cin >> x, tree[x].push_back(i);
+
+    cin >> color;
+    color.insert(color.begin(), '#');
+
+    dfs(1);
+
+    int cnt = 0;
+    for (int i = 1; i <= n; i++)
+        cnt += (subtreeColor[i].first == subtreeColor[i].second ? 1 : 0);
+
+    cout << cnt << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
