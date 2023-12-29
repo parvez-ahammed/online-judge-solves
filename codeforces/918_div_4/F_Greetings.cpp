8152022834b93e94c11adf198b31bc4ab76de36e
piru72 <parvezdirom2000@gmail.com>
2023-12-29 23:21:15 +0600

    PBDS

--- codeforces/918_div_4/F_Greetings.cpp
+++ codeforces/918_div_4/F_Greetings.cpp
@@ -2,40 +2,31 @@
 using namespace std;
 #define ll long long int
 #define endl "\n"
-
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 void solve()
 {
-
     int n;
     cin >> n;
-
     vector<pair<int, int>> v(n);
-    multiset<int> s;
-    for (int i = 0; i < n; i++) {
+    ordered_set<int> s;
+    for (int i = 0; i < n; i++)
         cin >> v[i].first >> v[i].second;
-        s.insert(v[i].second);
-    }
-
-    // for each element the answer will be the total element who have stated late and finished early
-
     sort(v.begin(), v.end());
-
-    ll cnt = 0;
-
-    for (int i = 0; i < n; i++) {
-
-        int currDes = v[i].second;
-
-        s.erase(s.find(currDes));
-
-        int contribute = distance(s.begin(), s.upper_bound(currDes));
-
-
-
-        cnt += contribute;
+    ll ans = 0;
+    for (auto x : v) {
+        s.insert(x.second);
     }
+    for (auto x : v) {
+        s.erase(s.find(x.second));
 
-    cout << cnt << endl;
+        // here order_of_key returns the number of elements strictly less than x.first
+        ans += s.order_of_key(x.second);
+    }
+    cout << ans << endl;
 }
 int32_t main()
 {
